fc7c704716c72caab97dc71cf28e11f519e47d3b
PhilRug <ruggiero.pj@gmail.com>
2023-06-14 14:08:33 -0400

    added resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,48 +8,48 @@ function ContactForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
+
     console.log('Form submitted!');
     console.log('Name:', name);
     console.log('Email:', email);
     console.log('Message:', message);
-    
+
   };
 
   return (
     <div className="body">
       <h2 className="title">Contact Me</h2>
       <div className='contactform'>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <h3>Name</h3>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <h3>Email</h3>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <h3>Message</h3>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea
-          id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-    </div>
     </div>
   );
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import '../styles/Pages.css';
 
+
+const techs = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'PWA', 'React', 'Bootstrap', 'APIs', 'Node', 'Express', 'MySQL, Sequelize', 'MongoDB, Mongoose', 'GraphQL', 'MERN']
+
 const Resume = () => {
   return (
     <div className="body">
       <h1 className='title'>Resume</h1>
+      <p><a href={resume}></a></p>
+      <ul>
+      <h2 className='title'>Technology Experience</h2>
+        {techs.map((name) => (
+          <li key={name}>{name}</li> 
+        ))}
+      </ul>
     </div>
   )
 }
