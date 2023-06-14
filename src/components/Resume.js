29627fcbee13065422670fd0bf5dc287d3ff2483
PhilRug <ruggiero.pj@gmail.com>
2023-06-14 14:17:16 -0400

    resume update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/Pages.css';
-
+import resume from '../Images/Philip_Ruggiero_Resume.pdf';
 
 const techs = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'PWA', 'React', 'Bootstrap', 'APIs', 'Node', 'Express', 'MySQL, Sequelize', 'MongoDB, Mongoose', 'GraphQL', 'MERN']
 
@@ -8,7 +8,7 @@ const Resume = () => {
   return (
     <div className="body">
       <h1 className='title'>Resume</h1>
-      <p><a href={resume}></a></p>
+      <p><a href={resume} download>Download My Resume</a></p>
       <ul>
       <h2 className='title'>Technology Experience</h2>
         {techs.map((name) => (
